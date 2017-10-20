f7af12858228aad5ef5c3bb54dfeec5710bd906d
Marcel Schneider <marcel@coopmasters.de>
2017-10-20 22:31:37 +0200

    compose: use constexpr

--- lib/compose.h
+++ lib/compose.h
@@ -23,25 +23,24 @@
 template<typename ... Fs>
 struct compose_impl
 {
-    compose_impl(Fs&& ... fs) : functionTuple(std::forward_as_tuple(fs ...)) {}
-
+    constexpr explicit compose_impl(Fs&& ... fs) : functionTuple(std::forward_as_tuple(fs ...)) {}
     template<std::size_t> struct int2type{};
 
     template<size_t N, typename ... Ts>
-    auto apply(int2type<N>, Ts&& ... ts)
+    constexpr auto apply(int2type<N>, Ts&& ... ts)
     {
         return std::get<N>(functionTuple)(apply(int2type<N-1>(),std::forward<Ts>(ts)...));
     }
 
     static const size_t size = sizeof ... (Fs);
     template<typename ... Ts>
-    auto apply(int2type<0>, Ts&& ... ts)
+    constexpr auto apply(int2type<0>, Ts&& ... ts)
     {
         return std::get<0>(functionTuple)(std::forward<Ts>(ts)...);
     }
 
     template<typename ... Ts>
-    auto operator()(Ts&& ... ts)
+    constexpr auto operator()(Ts&& ... ts)
     {
         return apply(int2type<sizeof ... (Fs) - 1>(), std::forward<Ts>(ts)...);
     }
@@ -50,7 +49,7 @@ struct compose_impl
 };
 
 template<typename ... Fs>
-auto compose(Fs&& ... fs)
+constexpr auto compose(Fs&& ... fs)
 {
     return compose_impl<Fs ...>(std::forward<Fs>(fs) ...);
 }
