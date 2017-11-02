ef895fa5267ff1a4be0a5111e300d42e6dc34f73
Marcel Schneider <marcel@coopmasters.de>
2017-11-02 23:27:19 +0100

    add constexpr

--- Tests/Test_MultiPredicate.cpp
+++ Tests/Test_MultiPredicate.cpp
@@ -49,20 +49,20 @@ namespace MultiPredicateTest {
                 (r1.carbon - r2.carbon) < 0.01 &&
                 (r1.price - r2.price) < 0.01;
     }
-    auto ImpactSulfur(float impact){
+    constexpr auto ImpactSulfur(float impact){
         return [impact](Res const& res){ return 1.0/(res.sulfur*impact);};
     }
-    auto ImpactCarbon(float impact){
+    constexpr auto ImpactCarbon(float impact){
         return [impact](Res const& res){ return res.carbon*impact;};
     }
-    auto ImpactPrice(float impact){
+    constexpr auto ImpactPrice(float impact){
         return [impact](Res const& res){ return 1.0/(res.price*impact);};
     }
     TEST_CASE("sort") {
 
         std::vector<Res> input = {{0.02, 0.4, 3.0}, {0.01, 0.4, 3.0}, {0.02, 0.3, 3.0}};
         std::vector<Res> expected = {{0.02, 0.3, 3.0},{0.02, 0.4, 3.0}, {0.01, 0.4, 3.0}, };
-        auto const quality = MultiPred(std::multiplies<float>(),
+        constexpr auto const quality = MultiPred(std::multiplies<float>(),
                                        ImpactSulfur(2.0),
                                        ImpactCarbon(1.0),
                                        ImpactPrice(1.0));
--- lib/MultiPredicate.h
+++ lib/MultiPredicate.h
@@ -13,29 +13,29 @@ class MultiPredImpl{
     BinaryOp binary_op;
     template<std::size_t> struct int2type {};
     template<size_t N, typename T>
-    auto apply(int2type<N>, T&& t)const {
+    constexpr auto apply(int2type<N>, T&& t)const {
         return binary_op(std::get<N>(preds_tuple)(std::forward<T>(t)),
                          apply(int2type<N - 1>(), std::forward<T>(t)));
     }
 
     template<typename T>
-    auto apply(int2type<0>, T&&  t)const {
+    constexpr auto apply(int2type<0>, T&&  t)const {
         return std::get<0>(preds_tuple)(std::forward<T>(t));
     }
 public:
-    MultiPredImpl(BinaryOp&& op, Predicates&& ... preds)
+    constexpr MultiPredImpl(BinaryOp&& op, Predicates&& ... preds)
     : preds_tuple(std::forward_as_tuple(preds ...))
      ,binary_op(std::forward<BinaryOp>(op)){}
 
     template<typename T>
-    auto operator()(T&& t)const {
+    constexpr auto operator()(T&& t)const {
         return apply(int2type<sizeof ... (Predicates)-1>(), std::forward<T>(t));
     }
 
 };
 
 template<typename BinaryOp , typename ... Predicates>
-auto MultiPred(BinaryOp &&op, Predicates &&... preds){
+constexpr auto MultiPred(BinaryOp &&op, Predicates &&... preds){
     return MultiPredImpl<BinaryOp, Predicates ...>(std::forward<BinaryOp>(op), std::forward<Predicates>(preds) ...);
 }
 
