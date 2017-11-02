0117e4afbbd5a0e2fed78b35ded2edb3bf677245
Marcel Schneider <marcel@coopmasters.de>
2017-11-02 22:55:37 +0100

    construct a configurable sort

--- Tests/Test_MultiPredicate.cpp
+++ Tests/Test_MultiPredicate.cpp
@@ -39,4 +39,36 @@ namespace MultiPredicateTest {
         REQUIRE(expected == input);
     }
 
+    struct Res{
+        float sulfur;
+        float carbon;
+        float price;
+    };
+    bool operator==(Res const& r1, Res const& r2){
+        return (r1.sulfur - r2.sulfur) < 0.01 &&
+                (r1.carbon - r2.carbon) < 0.01 &&
+                (r1.price - r2.price) < 0.01;
+    }
+    auto ImpactSulfur(float impact){
+        return [impact](Res const& res){ return 1.0/(res.sulfur*impact);};
+    }
+    auto ImpactCarbon(float impact){
+        return [impact](Res const& res){ return res.carbon*impact;};
+    }
+    auto ImpactPrice(float impact){
+        return [impact](Res const& res){ return 1.0/(res.price*impact);};
+    }
+    TEST_CASE("sort") {
+
+        std::vector<Res> input = {{0.02, 0.4, 3.0}, {0.01, 0.4, 3.0}, {0.02, 0.3, 3.0}};
+        std::vector<Res> expected = {{0.02, 0.3, 3.0},{0.02, 0.4, 3.0}, {0.01, 0.4, 3.0}, };
+        auto const quality = MultiPred(std::multiplies<float>(),
+                                       ImpactSulfur(2.0),
+                                       ImpactCarbon(1.0),
+                                       ImpactPrice(1.0));
+        std::sort(std::begin(input), std::end(input), [quality](Res const& r1, Res const& r2){ return quality(r1) < quality(r2);});
+
+        REQUIRE(expected == input);
+
+    }
 }
