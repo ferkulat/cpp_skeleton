e8520c37451dc54386c4ae77b81738e9cc21f5d6
Marcel Schneider <marcel@coopmasters.de>
2019-04-22 15:17:47 +0200

    added cmake module for extern header clara.hpp

--- CMake/clara.cmake
+++ CMake/clara.cmake
@@ -1,19 +1,18 @@
 cmake_minimum_required(VERSION 3.11 FATAL_ERROR)
 
 include(FetchContent)
-
 # ------------------------------------------------------------------------------
 # A modern, C++-native, header-only, test framework for unit-tests,
 # TDD and BDD - using C++11, C++14, C++17 and later
 FetchContent_Declare(
-  extern_catch2
+  extern_clara
   DOWNLOAD_NO_EXTRACT true
-  DOWNLOAD URL        https://github.com/catchorg/Catch2/releases/download/${EXTERN_CATCH_TAG}/catch.hpp
-  DOWNLOAD_DIR        ${EXTERN_CATCH_HEADER_DIR}
+  DOWNLOAD URL        https://github.com/catchorg/Clara/releases/download/${EXTERN_CLARA_TAG}/clara.hpp
+  DOWNLOAD_DIR        ${EXTERN_CLARA_HEADER_DIR}
 )
 
-FetchContent_GetProperties(extern_catch2)
-if(NOT extern_catch2_POPULATED)
-  FetchContent_Populate(extern_catch2)
+FetchContent_GetProperties(extern_clara)
+if(NOT extern_clara_POPULATED)
+  FetchContent_Populate(extern_clara)
 endif()
-include_directories(${EXTERN_CATCH_HEADER_DIR})
+include_directories(${EXTERN_CLARA_HEADER_DIR})
