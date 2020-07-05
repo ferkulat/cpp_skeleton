f5936607c421287c38ee948da927f7f36ceb9fbf
Marcel Schneider <marcel@coopmasters.de>
2020-07-05 19:56:24 +0200

    expose resulttype as external library

--- cmake/modules/CppResultType.cmake
+++ cmake/modules/CppResultType.cmake
@@ -1,14 +1,23 @@
 cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 include(FetchContent)
 
+set(component cppresulttype)
 
 FetchContent_Declare(
-  cppresulttype
+        extern_${component}
   GIT_REPOSITORY https://github.com/ferkulat/CppResultType.git
   GIT_TAG        6de4dfaa2a0d887deeaa6a329a0d2ed77b44280a
 )
-if(NOT cppresulttype_POPULATED)
-    FetchContent_Populate(cppresulttype)
+if(NOT extern_${component}_POPULATED)
+    FetchContent_Populate(extern_${component})
 endif()
-include_directories(SYSTEM ${cppresulttype_SOURCE_DIR}/include )
+add_library(${component}  INTERFACE)
+
+add_library(${component}::${component} ALIAS ${component})
+
+target_include_directories(${component}
+        INTERFACE
+        $<BUILD_INTERFACE:${extern_${component}_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+        )
 
