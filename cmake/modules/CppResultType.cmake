9d29c769bce0a7fc4b45fd1cf60e84afa4e37cca
Marcel Schneider <m.schneider@tkhsecurity.com>
2022-06-24 21:52:23 +0200

    update CppResultType

--- cmake/modules/CppResultType.cmake
+++ cmake/modules/CppResultType.cmake
@@ -1,23 +1,8 @@
 cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 include(FetchContent)
 
-set(component cppresulttype)
-
-FetchContent_Declare(
-        extern_${component}
+FetchContent_Declare(cppresulttype
   GIT_REPOSITORY https://github.com/ferkulat/CppResultType.git
-  GIT_TAG        6de4dfaa2a0d887deeaa6a329a0d2ed77b44280a
+  GIT_TAG        cd031e5be41b98876cb1d9d855838a045c624d62
 )
-if(NOT extern_${component}_POPULATED)
-    FetchContent_Populate(extern_${component})
-endif()
-add_library(${component}  INTERFACE)
-
-add_library(${component}::${component} ALIAS ${component})
-
-target_include_directories(${component}
-        INTERFACE
-        $<BUILD_INTERFACE:${extern_${component}_SOURCE_DIR}/include>
-        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-        )
-
+FetchContent_MakeAvailable(cppresulttype)
